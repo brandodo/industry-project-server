c8e89f44692342bc452f367ee588cd698f2062b6
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-05-08 17:53:51 -0400

    added the static image files

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ const booking = require("./routes/booking");
 
 app.use(cors());
 app.use(express.json());
-//app.use("/static", express.static("public"));
+
+app.use("/static/img1", express.static("public/Rectangle 3.jpg"));
+app.use("/static/img2", express.static("public/Rectangle 18.jpg"));
+app.use("/static/img3", express.static("public/Rectangle 500.jpg"));
+app.use("/static/img4", express.static("public/Rectangle 501.jpg"));
+app.use("/static/img5", express.static("public/Rectangle 502.jpg"));
+app.use("/static/img6", express.static("public/Rectangle 503.jpg"));
+app.use("/static/img7", express.static("public/unsplash_t1NEMSm1rgI-1.jpg"));
+app.use("/static/img8", express.static("public/unsplash_t1NEMSm1rgI.jpg"));
 app.use("/login", login);
 app.use("/booking", booking);
 
